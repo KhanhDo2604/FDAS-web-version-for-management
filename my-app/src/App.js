bb1fe714b14f4fcae10a39e01be363ab81ac216c
Vanboizz <102446711+Vanboizz@users.noreply.github.com>
2023-11-05 08:48:43 +0700

    List staff page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './pages/login/Login';
 import Listing from './pages/listing/Listing';
-import Updating from './pages/updating/Updating';
 import Adding from './pages/adding/Adding';
 
 function App() {
@@ -11,7 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Listing />} />
         <Route path="/login" element={<Login />} />
-        <Route path='/update' element={<Updating />} />
+        <Route path='/list' element={<Listing />} />
         <Route path='/add' element={<Adding />} />
       </Routes>
     </BrowserRouter>
--- my-app/src/pages/listing/Listing.jsx
+++ my-app/src/pages/listing/Listing.jsx
@@ -1,9 +1,212 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../listing/Listing.css"
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import ReactPaginate from 'react-paginate';
+
+const data = [
+    {
+        id: 1,
+        name: "Võ Đình Vân",
+        role: "IT",
+        attendanceTime: "20-11-2022",
+        gender: "Male",
+    },
+    {
+        id: 2,
+        name: "Trần Thị Thanh Bình",
+        role: "Content",
+        attendanceTime: "20-11-2022",
+        gender: "Female",
+    },
+    {
+        id: 3,
+        name: "Nguyễn Huỳnh Tuấn Khang",
+        role: "Media",
+        attendanceTime: "20-11-2022",
+        gender: "Male",
+    },
+    {
+        id: 4,
+        name: "Đỗ Phạm Huy Khánh",
+        role: "Markerting",
+        attendanceTime: "20-11-2022",
+        gender: "Male",
+    },
+    {
+        id: 1,
+        name: "Võ Đình Vân",
+        role: "IT",
+        attendanceTime: "20-11-2022",
+        gender: "Male",
+    },
+    {
+        id: 2,
+        name: "Trần Thị Thanh Bình",
+        role: "Content",
+        attendanceTime: "20-11-2022",
+        gender: "Female",
+    },
+    {
+        id: 3,
+        name: "Nguyễn Huỳnh Tuấn Khang",
+        role: "Media",
+        attendanceTime: "20-11-2022",
+        gender: "Male",
+    },
+    {
+        id: 4,
+        name: "Đỗ Phạm Huy Khánh",
+        role: "Markerting",
+        attendanceTime: "20-11-2022",
+        gender: "Male",
+    },
+    {
+        id: 1,
+        name: "Võ Đình Vân",
+        role: "IT",
+        attendanceTime: "20-11-2022",
+        gender: "Male",
+    },
+    {
+        id: 2,
+        name: "Trần Thị Thanh Bình",
+        role: "Content",
+        attendanceTime: "20-11-2022",
+        gender: "Female",
+    },
+    {
+        id: 3,
+        name: "Nguyễn Huỳnh Tuấn Khang",
+        role: "Media",
+        attendanceTime: "20-11-2022",
+        gender: "Male",
+    },
+    {
+        id: 4,
+        name: "Đỗ Phạm Huy Khánh",
+        role: "Markerting",
+        attendanceTime: "20-11-2022",
+        gender: "Male",
+    },
+    {
+        id: 2,
+        name: "Trần Thị Thanh Bình",
+        role: "Content",
+        attendanceTime: "20-11-2022",
+        gender: "Female",
+    },
+    {
+        id: 3,
+        name: "Nguyễn Huỳnh Tuấn Khang",
+        role: "Media",
+        attendanceTime: "20-11-2022",
+        gender: "Male",
+    },
+    {
+        id: 2,
+        name: "Trần Thị Thanh Bình",
+        role: "Content",
+        attendanceTime: "20-11-2022",
+        gender: "Female",
+    },
+    {
+        id: 3,
+        name: "Nguyễn Huỳnh Tuấn Khang",
+        role: "Media",
+        attendanceTime: "20-11-2022",
+        gender: "Male",
+    },
+    {
+        id: 2,
+        name: "Trần Thị Thanh Bình",
+        role: "Content",
+        attendanceTime: "20-11-2022",
+        gender: "Female",
+    },
+    {
+        id: 3,
+        name: "Nguyễn Huỳnh Tuấn Khang",
+        role: "Media",
+        attendanceTime: "20-11-2022",
+        gender: "Male",
+    },
+    {
+        id: 2,
+        name: "Trần Thị Thanh Bình",
+        role: "Content",
+        attendanceTime: "20-11-2022",
+        gender: "Female",
+    },
+    {
+        id: 3,
+        name: "Nguyễn Huỳnh Tuấn Khang",
+        role: "Media",
+        attendanceTime: "20-11-2022",
+        gender: "Male",
+    }
+]
 
 const Listing = () => {
+    const [startDate, setStartDate] = useState(new Date());
+    const [itemOffset, setItemOffset] = useState(0);
+    const itemsPerPage = 5;
+    const endOffset = itemOffset + itemsPerPage;
+    const currentItems = data.slice(itemOffset, endOffset);
+    const pageCount = Math.ceil(data.length / itemsPerPage);
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * itemsPerPage) % data.length;
+        setItemOffset(newOffset);
+    };
+
     return (
-        <div>Listing</div>
+        <div className='list'>
+            <div className='list__filter'>
+                <input placeholder='Search name of staff' />
+                <DatePicker
+                    selected={startDate}
+                    onChange={(date) => setStartDate(date)}
+                />
+            </div>
+            <div>
+                <div className='list__container'>
+                    <h5>Name</h5>
+                    <h5>Role</h5>
+                    <h5>Attendance Time</h5>
+                    <h5>Gender</h5>
+                    <h5>Action</h5>
+                </div>
+                <div>
+                    {
+                        currentItems && currentItems.map((value, index) => (
+                            <div key={index} className='list__item' >
+                                <p>{value.name}</p>
+                                <p>{value.role}</p>
+                                <p>{value.attendanceTime}</p>
+                                <p>{value.gender}</p>
+                                <div className='list__action'>
+                                    <button>
+                                        <img src='/pencil.png' alt='' />
+                                    </button>
+                                    <button>
+                                        <img src='/delete.png' alt='' />
+                                    </button>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+            <ReactPaginate
+                breakLabel="..."
+                nextLabel=">"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={5}
+                pageCount={pageCount}
+                previousLabel="<"
+                renderOnZeroPageCount={null}
+            />
+        </div>
     )
 }
 
