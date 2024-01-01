090dcc13afeb2375b07454e1d2ce12cda1ce345f
Đỗ Phạm Huy Khánh <20521442@gm.uit.edu.vn>
2024-01-01 11:31:08 +0700

    alert message 1

--- src/components/layout/UserHeader.jsx
+++ src/components/layout/UserHeader.jsx
@@ -1,53 +1,63 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import useUserImage from '../hooks/UseUserImage';
-import { UserAuth } from '../hooks/useAuth';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import useUserImage from "../hooks/UseUserImage";
+import { UserAuth } from "../hooks/useAuth";
 
 const UserHeader = ({ toggleSidebar }) => {
-    const { logOut, user } = UserAuth();
-    const url = useUserImage(user);
-    const navigate = useNavigate();
+  const { logOut, user } = UserAuth();
+  const url = useUserImage(user);
+  const navigate = useNavigate();
 
-    const handleLogout = () => {
-        logOut();
-        navigate("/login");
-    };
+  const handleLogout = () => {
+    logOut();
+    navigate("/login");
+  };
 
-    return (
-        <>
-            <div
-                style={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    padding: "10px",
-                }}
-            >
-                <img onClick={toggleSidebar} src="/Button.png" alt="" style={{width: '32px', cursor: 'pointer'}}/>
-                <div style={{ display: "flex", alignItems: "center", gap: "20px" }}>
-                    <img
-                        onClick={handleLogout}
-                        style={{width: '24px', cursor: 'pointer'}}
-                        src="/Logout.png"
-                        alt=""
-                    />
-                    {url ? (
-                        <img
-                            src={url}
-                            alt=""
-                            style={{ width: "40px", height: "40px", borderRadius: "50%" }}
-                        />
-                    ) : null}
-                </div>
-            </div>
-            <div
-                style={{
-                    width: "100%",
-                    height: "2px",
-                    boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-                }}
-            ></div>
-        </>
-    );
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          padding: "10px",
+        }}
+      >
+        {user.role != "staff" ? (
+          <img
+            onClick={toggleSidebar}
+            src="/Button.png"
+            alt=""
+            style={{ width: "32px", cursor: "pointer" }}
+          />
+        ) : (
+          <div style={{ width: "32px" }}></div>
+        )}
+
+        <div style={{ display: "flex", alignItems: "center", gap: "20px" }}>
+          <img
+            onClick={handleLogout}
+            style={{ width: "24px", cursor: "pointer" }}
+            src="/Logout.png"
+            alt=""
+          />
+          {url ? (
+            <img
+              src={url}
+              alt=""
+              style={{ width: "40px", height: "40px", borderRadius: "50%" }}
+            />
+          ) : null}
+        </div>
+      </div>
+      <div
+        style={{
+          width: "100%",
+          height: "2px",
+          boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+        }}
+      ></div>
+    </>
+  );
 };
 
 export default UserHeader;
